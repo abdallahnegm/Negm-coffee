6e6952aefd8d6d15f0520c7e19bcc35c45ff6538
abdallahnegm <73608714+abdallahnegm@users.noreply.github.com>
2023-07-05 04:40:49 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function()){
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function(){
+		$('.tm-nav').slideToggle();
+	});
+
+	$( window ).resize(function() {
+		if($( window ).width() > 767) {
+			$('.tm-nav').show();
+		} else {
+			$('.tm-nav').hide();
+		}
+	});}
